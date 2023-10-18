d1bb047088dd70f9b8a62f745a93b767d70c4f45
Ville Saalo <ville.saalo@iki.fi>
2023-10-18 14:04:26 +0300

    Add support for multi-module Maven projects

--- depstree2html.js
+++ depstree2html.js
@@ -1,5 +1,19 @@
 const readline = require('readline');
 
+console.log('<!DOCTYPE html>');
+console.log('<html lang="en">');
+console.log('<head><title>Maven dependency tree</title>');
+console.log('<style>');
+console.log('body { font-family: Arial, sans-serif; }');
+console.log('ul { list-style: none; padding-left: 2em; border-left: 1px solid #ccc; }');
+console.log('li { margin: 0.5em 0; }');
+console.log('a { text-decoration: none; color: blue; }');
+console.log('a:hover { text-decoration: underline; }');
+console.log('span.scope { color: grey; }');
+console.log('</style>');
+console.log('</head>');
+console.log('<body>');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -10,16 +24,25 @@ let root = {children: []};
 let stack = [{node: root, level: -1}];
 
 rl.on('line', function (line) {
-  const match = line.match(/([\s\\+\\\-]*)([\w\.\-]+):([\w\.\-]+):([\w\.\-]+):([\w\.\-]+):?(\w+)?$/);
-  if (match) {
+  const match = line.match(/INFO]([\s\\+\\\-|]*)([\w\.\-]+):([\w\.\-]+):([\w\.\-]+):([\w\.\-]+):?(\w+)?$/);
+
+  // Detect start of new module, output the previous one's contents, and reset the root:
+  if (line.startsWith('[INFO] Building')) {
+    const moduleName = line.split(' ')[2];  // Extract module name
+    generateHTML(root.children);
+    console.log(`<h1>${moduleName}</h1>`);
+    root = {children: []};
+    stack = [{node: root, level: -1}];
+
+  } else if (match) {
     const level = match[1].length;
     const groupId = match[2];
     const artifactId = match[3];
-    const version = match[4];
-    const packaging = match[5];
+    const packaging = match[4];
+    const version = match[5];
     const scope = match[6];
 
-    const newNode = {groupId, artifactId, version, packaging, scope, children: []};
+    const newNode = {groupId, artifactId, packaging, version, scope, children: []};
 
     while (stack.length > 0 && stack[stack.length - 1].level >= level) {
       stack.pop();
@@ -31,18 +54,7 @@ rl.on('line', function (line) {
 });
 
 rl.on('close', function () {
-  console.log('<html lang="en">');
-  console.log('<head><title>Maven dependency tree</title></head>');
-  console.log('<style>');
-  console.log('body { font-family: Arial, sans-serif; }');
-  console.log('ul { list-style: none; padding-left: 2em; border-left: 1px solid #ccc; }');
-  console.log('li { margin: 0.5em 0; }');
-  console.log('a { text-decoration: none; color: blue; }');
-  console.log('a:hover { text-decoration: underline; }');
-  console.log('span.scope { color: grey; }');
-  console.log('</style>');
-  console.log('</head>');
-  console.log('<body>');
+  // Output the contents of the final module, then close the file:
   generateHTML(root.children);
   console.log('</body>');
   console.log('</html>');
@@ -52,9 +64,9 @@ function generateHTML(nodes, indent = '') {
   if (nodes.length === 0) return;
   console.log(`${indent}<ul>`);
   for (const node of nodes) {
-    const url = `https://mvnrepository.com/artifact/${node.groupId}/${node.artifactId}/${node.version}/${node.packaging}`;
+    const url = `https://mvnrepository.com/artifact/${node.groupId}/${node.artifactId}/${node.version}`;
     const dependency = `${node.groupId}:${node.artifactId}:${node.version}:${node.packaging}`;
-    console.log(`${indent}  <li><a href="${url}" target="_blank">${dependency}</a><span class="scope">${!!node.scope ? ':' + node.scope : ''}</span></li>`);
+    console.log(`${indent}  <li><a href="${url}" target="_blank">${dependency}</a>${!!node.scope ? ':<span class="scope">' + node.scope + '</span>' : ''}</li>`);
     generateHTML(node.children, `${indent}  `);
   }
   console.log(`${indent}</ul>`);
